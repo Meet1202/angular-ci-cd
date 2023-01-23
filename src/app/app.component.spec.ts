cb3c2e571eab2b75375efba19330f8d2d8560abb
Meet Shah <41354599+Meet1202@users.noreply.github.com>
2023-01-23 11:36:21 +0530

    Solved test case.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-ci-cd app is deployed!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-ci-cd app is deployed.');
   });
 });
